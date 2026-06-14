a3c1c8ecfc59d5b4599697dce86541b4889593d1
Hemanth1102-hemu <hemanthdevarasetty@gmail.com>
2026-06-14 16:32:10 +0530

    Add temporary /debug-etl/ view to run and inspect ETL on Render

--- nifty_intel/urls.py
+++ nifty_intel/urls.py
@@ -17,9 +17,36 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+import traceback
+import sys
+from io import StringIO
+
+def debug_etl_view(request):
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    sys.stdout = mystdout = StringIO()
+    sys.stderr = mystderr = StringIO()
+    try:
+        from django.core.management import call_command
+        call_command('load_real_data', interactive=False)
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        return JsonResponse({"status": "success", "stdout": mystdout.getvalue(), "stderr": mystderr.getvalue()})
+    except Exception as e:
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        return JsonResponse({
+            "status": "error",
+            "error": str(e),
+            "stdout": mystdout.getvalue(),
+            "stderr": mystderr.getvalue(),
+            "traceback": traceback.format_exc()
+        })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('debug-etl/', debug_etl_view),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
